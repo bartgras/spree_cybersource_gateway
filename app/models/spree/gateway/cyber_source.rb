06ab0208b1904e24c76fb45f4175b2e918db168d
Jeff Squires <jeff.squires@gmail.com>
2012-06-21 13:58:20 -0400

    gets store & purchase working

--- app/models/spree/gateway/cyber_source.rb
+++ app/models/spree/gateway/cyber_source.rb
@@ -9,6 +9,30 @@ module Spree
       ActiveMerchant::Billing::CyberSourceGateway
     end
 
+    def authorize(amount, creditcard, gateway_options)
+#binding.pry
+      provider.authorize(amount, creditcard_or_reference(creditcard), gateway_options)
+    end
+
+    def purchase(amount, creditcard, gateway_options)
+      provider.purchase(amount, creditcard_or_reference(creditcard), gateway_options)
+    end
+
+    def capture(authorization, creditcard, gateway_options)
+#binding.pry
+      provider.capture(amount, creditcard_or_reference(creditcard), gateway_options)
+    end
+
+    def credit(amount, creditcard, response_code, gateway_options)
+#binding.pry
+      provider.refund(amount, identification, gateway_options)
+    end
+
+    def void(response_code, creditcard, gateway_options)
+#binding.pry
+      provider.void(response_code, gateway_options)
+    end
+
     def payment_profiles_supported?
       true
     end
@@ -32,16 +56,28 @@ module Spree
         options = options_for_create_customer_profile(creditcard, {})
 
         result = provider.store(creditcard, options)
+#binding.pry
 
         ## this is where i need to pick back up and figure out why i'm getting 'general failure' as well as what result params below will correspond to the correct ids
         if result.success?
-          creditcard.update_attributes(:gateway_customer_profile_id => result.params['???'], :gateway_payment_profile_id => result.params['???'])
+          # using 'subscriptionID' for both values since we only get one back from cybersource
+          creditcard.gateway_customer_profile_id = result.params['subscriptionID']
+          creditcard.gateway_payment_profile_id  = result.params['subscriptionID']
+          creditcard.save!
         else
           creditcard.gateway_error(result) if creditcard.respond_to? :gateway_error
         end
       end
     end
 
+    def creditcard_or_reference(creditcard)
+      # re: the 'dummy' below:
+      # the active_merchant class expects the creditcard referene number to be in this form:
+      #   reference_code, subscription_id, request_token = reference.split(";")
+      # which if goofy, b/c i don't have the outer pair available, so fake them (they are not used in the activemerchant 
+      # method anyways, they are thrown out
+      creditcard.gateway_payment_profile_id.present? ? "dummy;#{creditcard.gateway_payment_profile_id};dummy" : creditcard
+    end
 
     def options_for_create_customer_profile(creditcard, gateway_options)
       order = creditcard.payments.first.order
@@ -63,3 +99,4 @@ module Spree
 
   end
 end
+
